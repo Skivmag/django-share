9c5182ffaeeb20e7061fe7aa742dbecdbb2647ef
skivbard <skivbard@gmail.com>
2015-02-05 13:04:18 +0200

    pep8 fixes

--- share/templatetags/share_tags.py
+++ share/templatetags/share_tags.py
@@ -1,34 +1,40 @@
-import urlparse
-from django.template import Library, TemplateSyntaxError, Node
-from django.utils.http import urlquote
-from django.conf import settings
-from share.settings import *
+from django.template import Library, Node
 from django.template.loader import render_to_string
+
+from share.settings import *
+
 
 register = Library()
 
+
 @register.simple_tag
 def share_css():
-	return "<link href='" + settings.STATIC_URL +"css/share.css' type='text/css' rel='stylesheet' />"
+   return "<link href='" + settings.STATIC_URL + "css/share.css' type='text/css' rel='stylesheet' />"
+
 
 @register.simple_tag
 def share_js():
-	return "<script src='" + settings.STATIC_URL +"js/share.js' type='text/javascript'></script>"
+   return "<script src='" + settings.STATIC_URL + "js/share.js' type='text/javascript'></script>"
+
 
 class ShareNode(Node):
-  def __init__(self, providers=None):
-    self.providers = providers
-  def render(self, context):
-    return render_to_string('share/links.html', {'providers': self.providers, 'url': context['request'].build_absolute_uri()}, context_instance=context)
+   def __init__(self, providers=None):
+      self.providers = providers
+
+   def render(self, context):
+      return render_to_string('share/links.html',
+                              {'providers': self.providers, 'url': context['request'].build_absolute_uri()},
+                              context_instance=context)
+
 
 @register.tag
 def share(parser, token):
-  args = token.split_contents()
+   args = token.split_contents()
 
-  if len(args) == 1:
-    providers = SHARE_PROVIDERS
-  else:
-    args.pop(0)
-    providers = {'main': args }
+   if len(args) == 1:
+      providers = SHARE_PROVIDERS
+   else:
+      args.pop(0)
+      providers = {'main': args}
 
-  return ShareNode(providers)
+   return ShareNode(providers)
--- share/urls.py
+++ share/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
+
 from share.views import EmailFormView
 
+
 urlpatterns = patterns('',
-  url(r'^email/$', EmailFormView.as_view(), name='share_email'),
+                       url(r'^email/$', EmailFormView.as_view(), name='share_email'),
 )
